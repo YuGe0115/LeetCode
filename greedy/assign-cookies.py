655fa8cd05b01eaefcfb0fc00bd637641532162e
YuGe0115 <1724905827@qq.com>
2025-08-07 17:06:51 +0800

    LC-assign-cookies [Runtime: N/A; Memory: N/A]

---
+++ greedy/assign-cookies.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int: #定义一个函数，接收两个整数列表作为输入，并以一个整数作为输出
+    g.sort()
+    s.sort()
+
+
+    while chidren < chidren_num:
+
+        

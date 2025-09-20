ea1d0568b85af55d73c69db78eb18c4749744c6b
Tongyi Song <1724905827@qq.com>
2025-09-21 00:46:41 +0800

    LC-two-sum-ii-input-array-is-sorted [Runtime: N/A; Memory: N/A]

---
+++ binary-search/two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        #使用相向双指针思想，降低时间复杂度，主要思想是提升计算的效率
+        left = 0
+        right = len(numbers)
+        while left < right:
+            if numbers[left] + numbers[right] > target:
+                right -= right
+            elif numbers[left] + numbers[right] < target:
+                left += left
+            elif numbers[left] + numbers[right] == target:
+                break
+        return [left+1, right]
+        

a10352b70360fa005a658ee4a00faccab6254346
Tongyi Song <1724905827@qq.com>
2025-10-01 01:11:12 +0800

    LC-roman-to-integer [Runtime: 1 ms; Memory: 17.7 MB]

--- string/roman-to-integer.py
+++ string/roman-to-integer.py
@@ -1,27 +1,12 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman_values = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
-
-        result = 0
-
-        for i in range(len(s) - 1):
-            current_value = roman_values[s[i]]
-            next_value = roman_values[s[i + 1]]
-            
-            if current_value < next_value:
-                result -= current_value
-
+        values = {"I": 1, "V" : 5, "X" : 10, "L" : 50, "C": 100, "D" : 500, "M" : 1000}
+        output = 0
+        for i in range(len(s)):
+            output = output + values[s[i]]
+        for i in range(len(s)-1):
+            if values[s[i]] < values[s[i+1]]:
+                output = output - 2*values[s[i]]
             else:
-                result += current_value
-
-        result += roman_values[s[-1]]
-        
-        return result
+                output
+        return output

8c637138bea899de95d7108f8b60a1a2bdf9be44
Tongyi Song <1724905827@qq.com>
2025-10-02 23:56:12 +0800

    LC-two-sum [Runtime: 0 ms; Memory: 19.1 MB]

--- hash-table/two-sum.py
+++ hash-table/two-sum.py
@@ -1,9 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}  # 键：数值，值：下标
+        hashmap = {}  # 字典
         for i, num in enumerate(nums):
             complement = target - num
             if complement in hashmap:
                 return [hashmap[complement], i]
             hashmap[num] = i  # 存入当前元素
-        return []
